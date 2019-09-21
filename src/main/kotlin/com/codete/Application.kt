1a22c4a1fa73b4b911685ef079dabf0aab52b3f5
krrybarc <krystian.rybarczyk@icloud.com>
2019-09-21 11:23:47 +0200

    Added code for the next slide

--- src/main/kotlin/com/codete/Application.kt
+++ src/main/kotlin/com/codete/Application.kt
@@ -1,12 +1,36 @@
 package com.codete
 
+import com.codete.Spaceship.Companion.reportCleaning
+
+val spaceship = Spaceship()
 
 fun main() {
+    try {
+        initiateStart1()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 
-    val spaceship = Spaceship()
+    try {
+        initiateStart2()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 
+}
+
+fun initiateStart1() {
     val engineFuture = spaceship.startEngines()
     val cleaningFuture = spaceship.startAutoCleaning()
 
-    engineFuture.whenComplete { _, failure -> if (failure == null) spaceship.calculateShortestPathToNextStation() }
+    engineFuture.whenComplete { speed, failure -> if (failure == null) spaceship.setOff(speed) }
+    cleaningFuture.whenComplete { result, failure -> if (failure == null) reportCleaning(result) }
+}
+
+fun initiateStart2() {
+    spaceship.startEngines()
+        .thenCombine(spaceship.startAutoCleaning()) { speed, two ->
+            spaceship.setOff(speed)
+            two
+        }.whenComplete { result, failure -> if (failure == null) reportCleaning(result) }
 }
--- src/main/kotlin/com/codete/Spaceship.kt
+++ src/main/kotlin/com/codete/Spaceship.kt
@@ -2,22 +2,30 @@ package com.codete
 
 import java.util.concurrent.CompletableFuture
 
+
 class Spaceship {
 
     //    val engines: Map<Int, Engine> = (1..ENGINES_COUNT).zip(generateSequence { Engine() }.take(ENGINES_COUNT).toList()).toMap()
-    fun startEngines(): CompletableFuture<Unit> {
-        return CompletableFuture()
+    fun startEngines(): CompletableFuture<Int> {
+//        Thread.sleep(5000)
+        return CompletableFuture.supplyAsync { 120 }
     }
 
     fun startAutoCleaning(): CompletableFuture<Result> {
-        return CompletableFuture()
+        val completableFuture = CompletableFuture<Result>()
+        completableFuture.completeExceptionally(RuntimeException())
+        return completableFuture
     }
 
-    fun calculateShortestPathToNextStation(): CompletableFuture<List<Int>> {
-        return CompletableFuture()
+    fun setOff(speed: Int) {
+        println("Spaceship is taking off with the initial speed of $speed")
     }
 
     companion object {
         const val ENGINES_COUNT = 5
+
+        fun reportCleaning(result: Result?) {
+            println("Cleaning completed with result $result")
+        }
     }
 }

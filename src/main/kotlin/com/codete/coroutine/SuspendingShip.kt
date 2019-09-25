180b0707595a5e1cb551e094a8597772e635fa53
krrybarc <krystian.rybarczyk@icloud.com>
2019-09-25 11:44:47 +0200

    Make suspending functions actually suspendable

--- src/main/kotlin/com/codete/coroutine/SuspendingShip.kt
+++ src/main/kotlin/com/codete/coroutine/SuspendingShip.kt
@@ -1,16 +1,19 @@
 package com.codete.coroutine
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
+@Suppress("IMPLICIT_NOTHING_AS_TYPE_PARAMETER")
 class SuspendingShip {
 
-    suspend fun startEngines(): Int {
+    suspend fun startEngines(): Int = withContext(Dispatchers.IO){
         println("Starting engines")
         delay(700)
-        return 190
+        190
     }
 
-    suspend fun autoClean(): Boolean {
+    suspend fun autoClean(): Boolean = withContext(Dispatchers.IO) {
         println("Starting cleaning")
         delay(500)
         throw RuntimeException()
